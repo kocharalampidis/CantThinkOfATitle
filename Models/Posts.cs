7044bdf134200e8535eaf44fb9ede4fa09f59dde
kocharalampidis <kocharalampidis@gmail.com>
2023-11-21 13:24:13 +0200

    initializing project with basic setup and db

---
+++ Models/Posts.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CantThinkOfATitle.Models
+{
+    public class Posts
+    {
+        //private int Id { get; set; }
+        [Key]
+        public int Id { get; set; }
+    
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+    }
+}
+
+//"userId": 1,
+//    "id": 1,
+//    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//    "body"
--- Models/User.cs
+++ Models/User.cs
@@ -2,17 +2,17 @@
 {
     public class User
     {
-        private int Id { get; set; }
+        public int Id { get; set; }
 
-        private string Name { get; set; }
+        public string Name { get; set; }
 
-        private string Email { get; set; }
+        public string Email { get; set; }
 
-        private string Phone { get; set; }
+        public string Phone { get; set; }
 
        // private string Password { get; set; }
 
-    }
+    } 
 }
 
 //"id": 1,
