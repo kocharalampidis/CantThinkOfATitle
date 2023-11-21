ecd3243be6ab6ee9544988377895caca863b1355
kocharalampidis <kocharalampidis@gmail.com>
2023-11-21 10:15:27 +0200

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,32 @@
+﻿namespace CantThinkOfATitle.Models
+{
+    public class User
+    {
+        private int Id { get; set; }
+
+        private string Name { get; set; }
+
+        private string Email { get; set; }
+
+        private string Phone { get; set; }
+
+       // private string Password { get; set; }
+
+    }
+}
+
+//"id": 1,
+//    "name": "leanne graham",
+//    "username": "bret",
+//    "email": "sincere@april.biz",
+//    "address": {
+//    "street": "kulas light",
+//      "suite": "apt. 556",
+//      "city": "gwenborough",
+//      "zipcode": "92998-3874",
+//      "geo": {
+//        "lat": "-37.3159",
+//        "lng": "81.1496"
+//      }
+//},
+//    "phone": "1-770-736-8031 x56442",///
